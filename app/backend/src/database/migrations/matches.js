29c318b3016dc7d843ec49d36ab1a18babcf29c1
Willian <willian.alves.b15@gmail.com>
2022-07-07 15:49:39 -0300

    feat: criando migrations matches

--- app/backend/src/database/migrations/matches.js
+++ app/backend/src/database/migrations/matches.js
@@ -2,25 +2,37 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
   await queryInterface.createTable('matches', {
       id:{
-          primaryKey: true,
-          autoIncrement: true,
-          type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
       },
       home_team: {
-          type: Sequelize.INTEGER,
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'teams',
+          key: 'id'
+        }
       },
       home_team_goals: {
-          type: Sequelize.INTEGER,
+        type: Sequelize.INTEGER,
+        allowNull: false,
       },
       away_team: {
-          type: Sequelize.INTEGER,
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'teams',
+          key: 'id'
+        }
       },
       away_team_goals: {
-          type: Sequelize.INTEGER,
+        type: Sequelize.INTEGER,
+        allowNull: false,
       },
       in_progress: {
-          type: Sequelize.BOOLEAN,
-          defaultValue: false,
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
       },
   });
 },
