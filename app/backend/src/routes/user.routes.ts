c609aba7f286c9116a2f1f2ef7ddf5964be4bb95
Willian <willian.alves.b15@gmail.com>
2022-07-11 16:44:29 -0300

    feat: added middleware validation login in routes user

--- app/backend/src/routes/user.routes.ts
+++ app/backend/src/routes/user.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { UserFactory } from '../factory';
+import { validationEmail } from '../middleware/handleLogin';
 
 const userRouter = Router();
 
-userRouter.post('/login', (req, res, next) => {
+userRouter.post('/login', validationEmail, (req, res, next) => {
   UserFactory().login(req, res, next);
 });
 
