6c752533cfa5054b69b2c40d1811fae679291241
Willian <willian.alves.b15@gmail.com>
2022-07-11 19:45:17 -0300

    feat: created route /login/validate with func role

--- app/backend/src/routes/user.routes.ts
+++ app/backend/src/routes/user.routes.ts
@@ -8,4 +8,8 @@ userRouter.post('/login', validationEmail, validationPassword, (req, res, next) 
   UserFactory().login(req, res, next);
 });
 
+userRouter.get('/login/validate', (req, res, next) => {
+  UserFactory().role(req, res, next);
+});
+
 export default userRouter;
