e8ec312abcde8c13bf3f48ea05c0c55100ce690c
Willian <willian.alves.b15@gmail.com>
2022-07-11 16:43:14 -0300

    feat: created middleware validation email

---
+++ app/backend/src/middleware/handleLogin.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from 'express';
+
+export function validationEmail(req: Request, res: Response, next: NextFunction) {
+  try {
+    const { email } = req.body;
+    if (!email) {
+      return res.status(400).json({ message: 'All fields must be filled' });
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
+
+export const lest = {};
