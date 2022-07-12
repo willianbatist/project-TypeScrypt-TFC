446384785eba27d1b85798cf7708188a3adec137
Willian <willian.alves.b15@gmail.com>
2022-07-11 22:44:13 -0300

    feat: created route get id teams

--- app/backend/src/routes/team.routes.ts
+++ app/backend/src/routes/team.routes.ts
@@ -7,4 +7,8 @@ teamRouter.get('/teams', (req, res, next) => {
   TeamFactory().findAll(req, res, next);
 });
 
+teamRouter.get('/teams/:id', (req, res, next) => {
+  TeamFactory().findOne(req, res, next);
+});
+
 export default teamRouter;
