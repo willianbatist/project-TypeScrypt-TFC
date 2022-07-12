c42c947284f7cad8c27c5c4d53b16d31f496b64c
Willian <willian.alves.b15@gmail.com>
2022-07-12 18:27:16 -0300

    feat: created interfaces IMatch, IMatchModel and IMatchService

--- app/backend/src/protocols/index.ts
+++ app/backend/src/protocols/index.ts
@@ -34,3 +34,20 @@ export interface ITeamService {
   findAll(): Promise<ITeam[] | null>;
   findOne(id: string): Promise<ITeam | null>;
 }
+
+export interface IMatch {
+  id?: number;
+  homeTeam: number;
+  homeTeamGoals: number;
+  awayTeam: number;
+  awayTeamGoals: number;
+  inProgress: boolean;
+}
+
+export interface IMatchModel {
+  findAll(inProgress: boolean | null): Promise<IMatch[] | null>;
+}
+
+export interface IMatchService {
+  findAll(inProgress: boolean | null): Promise<IMatch[] | null>;
+}
