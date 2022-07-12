40cfff5273cf186559327e7950a75ae94b0adc27
Willian <willian.alves.b15@gmail.com>
2022-07-12 18:28:25 -0300

    feat: created repository match, created class match repository with findAll

---
+++ app/backend/src/repository/match.repository.ts
@@ -0,0 +1,26 @@
+import Model from '../database/models/Match';
+import { IMatchModel, IMatch } from '../protocols/index';
+import teamModel from '../database/models/Team';
+
+export default class MatchRepository implements IMatchModel {
+  constructor(private model = Model) {
+    this.model = model;
+  }
+
+  async findAll(inProgress: boolean | null): Promise<IMatch[] | null> {
+    let totalMatches: unknown;
+    if (inProgress === null) {
+      totalMatches = await this.model.findAll({ include: [
+        { model: teamModel, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: teamModel, as: 'teamAway', attributes: { exclude: ['id'] } },
+      ] });
+    } else {
+      totalMatches = await this.model.findAll({ where: { inProgress },
+        include: [
+          { model: teamModel, as: 'teamHome', attributes: { exclude: ['id'] } },
+          { model: teamModel, as: 'teamAway', attributes: { exclude: ['id'] } },
+        ] });
+    }
+    return totalMatches as IMatch[];
+  }
+}
