106805e0ef311aef61cd0423b4c467586ac811e5
Willian <willian.alves.b15@gmail.com>
2022-07-13 17:58:49 -0300

    feat: added validateMatches in routes matches

--- app/backend/src/routes/match.routes.ts
+++ app/backend/src/routes/match.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { MatchFactory } from '../factory/index';
 import validateJWT from '../middleware/validateJWT';
+import validationMatch from '../middleware/validateMatches';
 
 const matchRouter = Router();
 
@@ -8,11 +9,11 @@ matchRouter.get('/matches', (req, res, next) => {
   MatchFactory().findAll(req, res, next);
 });
 
-matchRouter.post('/matches', validateJWT, (req, res, next) => {
+matchRouter.post('/matches', validateJWT, validationMatch, (req, res, next) => {
   MatchFactory().create(req, res, next);
 });
 
-matchRouter.patch('/matches/:id/finish', (req, res, next) => {
+matchRouter.patch('/matches/:id/finish', validationMatch, (req, res, next) => {
   MatchFactory().update(req, res, next);
 });
 
