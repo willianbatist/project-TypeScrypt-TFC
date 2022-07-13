6b4c285064eb9fbd1b70e2baf93725d963b9e2a3
Willian <willian.alves.b15@gmail.com>
2022-07-13 16:26:33 -0300

    fix: refactoring middleware validateJWT

--- app/backend/src/middleware/validateJWT.ts
+++ app/backend/src/middleware/validateJWT.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, Response } from 'express';
+import 'dotenv/config';
+import * as jwt from 'jsonwebtoken';
 
 const validateJWT = async (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers.authorization;
   try {
-    if (!token) {
-      return res.status(401).json({ message: 'Token must be a valid token' });
-    }
+    const token = req.headers.authorization as string;
+    const JWT: string = process.env.JWT_SECRET || 'jwt_secret';
+    jwt.verify(token, JWT) as jwt.JwtPayload;
     return next();
   } catch (error) {
     return res.status(401).json({ message: 'Token must be a valid token' });
