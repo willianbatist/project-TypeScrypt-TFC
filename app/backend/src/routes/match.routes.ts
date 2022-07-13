6204158d6d374ddbe0f549f1f91b881aa8a4da97
Willian <willian.alves.b15@gmail.com>
2022-07-12 22:51:31 -0300

    feat: created route post / metches

--- app/backend/src/routes/match.routes.ts
+++ app/backend/src/routes/match.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import { MatchFactory } from '../factory/index';
+import validateJWT from '../middleware/validateJWT';
 
 const matchRouter = Router();
 
-matchRouter.get('/matches', (req, res, next) => {
+matchRouter.get('/matches', validateJWT, (req, res, next) => {
   MatchFactory().findAll(req, res, next);
+});
+
+matchRouter.post('/matches', validateJWT, (req, res, next) => {
+  MatchFactory().create(req, res, next);
 });
 
 export default matchRouter;
