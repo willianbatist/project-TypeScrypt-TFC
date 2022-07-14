5e4b02fb275ef89d5a9b345b4844c26603d990ba
Willian <willian.alves.b15@gmail.com>
2022-07-13 21:57:29 -0300

    fix: error validateMatches

--- app/backend/src/middleware/validateMatches.ts
+++ app/backend/src/middleware/validateMatches.ts
@@ -1,14 +1,17 @@
 import { NextFunction, Request, Response } from 'express';
+import TeamRepository from '../repository/team.repository';
 
-export default function validationMatch(req: Request, res: Response, next: NextFunction) {
-  try {
-    const { homeTeam, awayTeam } = req.body;
-    if (homeTeam === awayTeam) {
-      return res.status(401)
-        .json({ message: 'It is not possible to create a match with two equal teams' });
-    }
-    next();
-  } catch (error) {
-    next(error);
+const teams = new TeamRepository();
+
+export default async function validationMatch(req: Request, res: Response, next: NextFunction) {
+  const { homeTeam, awayTeam } = req.body;
+  const checkTeams = await teams.findTeam(homeTeam, awayTeam);
+  if (homeTeam === awayTeam) {
+    return res.status(401)
+      .json({ message: 'It is not possible to create a match with two equal teams' });
   }
+  if (checkTeams?.length !== 2) {
+    return res.status(404).json({ message: 'There is no team with such id!' });
+  }
+  next();
 }
